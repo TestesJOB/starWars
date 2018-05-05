87a77dd7fdb9cdd8e2f1cacbb8bd888e6dbb5a22
maraoli <mara.15oliveira@outlook.com>
2018-05-05 09:27:21 -0300

    compiloouoou!!!

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,59 +1,52 @@
 import * as axios from "axios";
 import * as React from 'react';
-
 // import React, { Component } from 'react';
-// import { withRouter } from 'react-router'
+
 import {
    BrowserRouter as Router,
    Link,
-   Route,
-   Switch
+   Route
 } from 'react-router-dom'
 
 // CSS
 import "./css/App.css";
 
-// FILES
-// import Home from "./components/pages/Home";
-
 const baseSWP = "https://swapi.co/api/people/";
+const personagem = '';
 
-class App extends React.Component {
+class App extends React.Component<any, any> {
     
   constructor(props:any){
     super(props);
 
     this.state = {
-        personagens: []
+        personagem: []
     }
   }
 
   public componentDidMount(){
     const self = this;
-    axios.default.get(baseSWP)
+    axios.default.get(baseSWP + "1")
     .then(response => {
-        // console.log(response.data);
         self.setState({
-            personagens: response.data
+            personagem: response.data
         })
     })
     .catch(error => {
-        // console.log(error);
-        // console.log(error.status);
+        // erro
     });
   }
 
   public render() {
     return (
-      <div className="App">
-          < Router >
-              <div>TESTE 1</div>
-          </ Router >
-          < Router >
-              <div>TESTE 2</div>
-          </ Router >
-          {/* <Route exact path="/" component={Home}/> */}
-      </div>
+        <React.Fragment>
+            < Router >
+                <div>TESTE 1</div>
+            </ Router >
+            < Router >
+                <div>{this.state.personagem.name}</div>
+            </ Router >
+        </React.Fragment>
     );
   }
 }
