4e7f77ae320fdffb266b85f33187fe4a19373438
maraoli <mara.15oliveira@outlook.com>
2018-05-06 10:30:58 -0300

    Alt

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -14,31 +14,10 @@ import "./css/App.css";
 // PAGES
 import Home from "./pages/Home";
 
-// VARS
-const baseSWP = "https://swapi.co/api/people/";
-const personagem = '';
-
 class App extends React.Component<any, any> {
     
   constructor(props:any){
     super(props);
-
-    this.state = {
-        personagem: []
-    }
-  }
-
-  public componentDidMount(){
-    const self = this;
-    axios.default.get(baseSWP + "2")
-    .then(response => {
-        self.setState({
-            personagem: response.data
-        })
-    })
-    .catch(error => {
-        // erro
-    });
   }
 
   public render() {
@@ -47,16 +26,8 @@ class App extends React.Component<any, any> {
             <Router>
                 <Switch>
                     <Route exact={true} path="/" component={Home} />
-                    <Route exact={true} path="/sds" component={Home} />
                 </Switch>
             </Router>
-            <Home/>
-            < Router >
-                <div>TESTE 1</div>
-            </ Router >
-            < Router >
-                <div>{this.state.personagem.name}</div>
-            </ Router >
         </React.Fragment>
     );
   }
